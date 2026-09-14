91b41a23b59c9dba073dbbb311311ad29db8226b
GEAR ENCORE <ila289193@gmail.com>
2026-09-14 09:24:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
-"""Основной файл приложения
-    версия 0.0.2
-"""
 from random import choice
 
-collection = ['task1', 'task2', 'task3']
-is_start = True
+collection = ["Oreshki BIG BOB","Taraska" ]
+is_running = True
 
-while (is_start):
-    print("2 - показать задачи \ 1 - добавить заметку")
-    choice_user = input('введите ваш выбор (1 или 2)')
-    match int(choice_user):
-        case 1:
-            print(collection[0])
-        case 2:
-            collection.append('task5')
-            print(collection)
+def show_collection(task_collection):
+    print("============================")
+    for i, j in enumerate(task_collection):
+        print(i, j)
+    print("============================")
+
+def show_menu():
+    print("1 - посмотреть задачи \n"
+          "2 - добавить задачи\n"
+          "3 - редактировать задачу \n"
+          "4 - удаление задачи \n"
+          "5 - Выход \n")
+while is_running:
+    show_menu()
+    choice_user = input('Введите ваш выбор - ')
+
+    match str(choice_user):
+        case '1':
+            show_collection(collection)
+        case '2':
+            print("============================")
+            add_task = input("Введите имя задачи для создания - ")
+            collection.append(add_task)
+        case '3' :
+            print("============================")
+            show_collection(collection)
+            print("============================")
+            select_task = int(input("Введите номер задачи для редактирования - "))
+            print("============================")
+            edit_task = input("Введите новое имя выбранной задачи для редактирования - ")
+            collection[select_task - 1] = edit_task
+        case '4':
+            show_collection(collection)
+            delete_task = int(input("Введите номер задачи для удаления - "))
+            collection.pop(delete_task - 1)
+        case '5':
+            is_running = False
+            print("============================")
+            (print("До свидания!"))
+            print("============================")
         case _:
-            print('такого пункта нет...')
+           print("Такого пункта нет!")
